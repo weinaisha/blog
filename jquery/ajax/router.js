ddd0b7927baf3df57ddb6ed372f29c8631f95baf
weizhazha <1163504612@qq.com>
2017-06-23 23:56:04 +0800

    6.23

---
+++ jquery/ajax/router.js
@@ -0,0 +1,39 @@
+router.get('/getNews', function(req, res) {
+  var news=[
+    {
+      title: '李克强：如果沿用老办法管制就可能没有今天的微信',
+      date: '2016-6-22'
+    },
+    {
+      title:"所罗门矩阵调查：这可能是中国互联网史上最大的骗局",
+      date:"2016-6-22"
+    },
+    {
+      title:"为什么家用选择220V电路，而不是380V？原来两者的区别是",
+      date:"2016-6-22"
+    },
+    {
+      title:"原来银行人都是这样设置密码的！",
+      date:"2016-6-22"
+    },
+    {
+      title:"腾讯究竟是哪个国家的公司？",
+      date:"2016-6-22"
+    },
+    {
+      title:"丁宁4:0完胜韩国名将梁夏银 一齐弯腰找球场景遭调侃",
+      date:"2016-6-22"
+    },
+    {
+      title:"奥运会已成烫手山芋，众国皆躲，奥组委希望中国接锅",
+      date:"2016-6-22"
+    }
+  ];
+  var len=2;
+  var pageIndex=req.query.page;
+  var retNews=news.slice(pageIndex*len,len*pageIndex+len);
+  res.send({
+    status:1,
+    data:retNews
+  });
+});
---
+++ jquery/getWeather/main.js
@@ -0,0 +1,106 @@
+var currentCity='';
+
+$('document').ready(function(){
+  $('.tips').on('mouseenter','.unfloded',function(){
+    $(this).parents('.tips-panel').find('.detail').slideDown();
+  });
+  $('.tips').on('mouseleave','.unfloded',function(){
+    $(this).parents('.tips-panel').find('.detail').slideUp();
+  });
+  $('.taday-weather').on('click','.inquire',function(){
+    currentCity=$('.select-city').val();
+    getWeather(currentCity);
+  });
+  getWeather(currentCity);
+  // '.taday-weather'
+});
+
+  function getWeather(currentCity){
+    if (!currentCity) {
+      $.get('http://api.jirengu.com/city.php').done(function(ret){
+        if (ret) {
+          currentCity=ret;
+        }else{
+          alert("获取不到当前城市信息！");
+        }
+      }).fail(function(){
+        alert("系统错误！");
+      })
+    }
+    $.get('//jirenguapi.applinzi.com/weather.php?',{city:currentCity}).done(function(ret){
+      var data=JSON.parse(ret);
+      if (data.error===0&&data.status==='success') {
+        renderPage(data.results);
+        $('.select-city').focus();
+        $('.select-city').keydown(function(event){
+          if (event.which==13) {
+            currentCity=$('.select-city').val();
+            getWeather(currentCity);
+          }
+        });
+      }else{
+        alert("请输入正确的城市名称");
+      }
+    }).fail(function(){
+      alert("系统错误");
+    })
+  }
+  
+function renderPage(data){
+    var data=data[0]
+    var weather_data=data.weather_data;
+    var currentCity=data.currentCity;
+    var index=data.index;
+    var pm25=data.pm25;
+    var tipsHtml='';
+    var forecastHtml='';
+    var taday_weatherHtml='';
+  weather_data.forEach(function(item,index){
+    if (index===0){
+      taday_weatherHtml= '<div class="weather">'+
+                            '<img src="'+item.dayPictureUrl+'">'+
+                            '<p>'+item.weather+'</p>'+
+                            '<p>'+item.temperature+'</p>'+
+                            '<p>'+item.wind+'</p>'+
+                          '</div>'+
+                          '<div class="information">'+
+                            '<ul>'+
+                              '<li>'+
+                                '<input class="select-city" type="text" name="city" placeholder="切换城市">'+
+                                '<input type="button" class="inquire" value="查询">'+
+                              '</li>'+
+                              '<li>'+
+                                '<h3 class="city">'+currentCity+'</h3>'+
+                              '</li>'+
+                              '<li>'+item.date+'</li>'+
+                              '<li>pm25:'+pm25+'</li>'+
+                            '</ul>'+
+                          '</div>';
+      $('.taday-weather').html(taday_weatherHtml);
+    }
+    if (index>0) {
+      forecastHtml+='<li class="forecast-panel">'
+                        +'<ul>'
+                          +'<li>'+item.date+'</li>'
+                          +'<li>'
+                            +'<img src="'+item.dayPictureUrl+'">'
+                          +'</li>'
+                          +'<li>'+item.weather+'</li>'
+                          +'<li>'+item.temperature+'</li>'
+                          +'<li>'+item.wind+'</li>'
+                        +'</ul>'
+                      +'</li>';
+    }
+  })
+  $('.forecast').html(forecastHtml);
+  index.forEach(function(item,index){
+    tipsHtml+= '<li class="tips-panel">'
+                +'<ul>'
+                  +'<li>'+item.title+'</li>'
+                  +'<li class="unfloded">'+item.zs+'</li>'
+                  +'<li class="detail">'+item.des+'</li>'                      
+                +'</ul>'
+              +'</li>';
+  })
+  $('.tips').html(tipsHtml);
+}
